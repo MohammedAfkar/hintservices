1b1833b92c6619cbb180b5a82d7064b33aec5fb3
Mohammed Afkar <afkarabi34@gmail.com>
2024-01-03 22:34:45 +0530

    review dp changed, stylesheet organized

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,20 +4,4 @@ function send() {
     var msg = url +
         "Hi, Hint services";
     window.open(msg, '_blank').focus();
-}
-
-/*
-function sendMail() {
-
-    let params = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        mobile: document.getElementById('mobile').value,
-        message: document.getElementById('message').value
-    }
-
-    var serviceID = "#";
-    var templateID = "#";
-    
-    emailjs.send(serviceID, templateID, params).then(alert("Thank you for choosing HINT SERVICES! Your request has been received, an email confirming your booking details will arrive shortly. If you have any immediate queries, feel free to reach out. Have a wonderful day!"));
-}*/
+};
